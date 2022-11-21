af84809ed93e7d0e71149fb43739e958993a892a
SalmaRagab <salmaahragab@gmail.com>
2022-11-21 23:38:02 +0100

    add create project route and validations in the project model

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -3,4 +3,21 @@ class ProjectsController < ApplicationController
         projects = Project.all
         render json: projects
     end
+
+    def create
+        project = Project.new(project_params)
+        if project.save
+            render json: project
+        else
+            render json: {
+                reasons: project.errors.full_messages
+            }, status: :unprocessable_entity
+        end
+    end
+
+    private
+
+    def project_params
+        params.require(:project).permit(:name, :owner_id, :state)
+    end
 end
--- app/models/project.rb
+++ app/models/project.rb
@@ -1,3 +1,5 @@
 class Project < ApplicationRecord
+    validates :name, :state, :owner_id, presence: true
+    validates_length_of :name , maximum: 255
     enum status: { planned: 0, active: 1, done: 2, failed: 3 }
 end
