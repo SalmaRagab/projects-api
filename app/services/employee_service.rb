164d1caf7a303023f121aefba9239a932c321610
SalmaRagab <salmaahragab@gmail.com>
2022-11-22 00:30:12 +0100

    add external employee service

---
+++ app/services/employee_service.rb
@@ -0,0 +1,6 @@
+class EmployeeService
+    def self.get_employee_by_id id
+        response =  RestClient.get "https://employees-api.vercel.app/api/employees/#{id}"
+        employee = JSON.parse(response.body)
+    end
+end
