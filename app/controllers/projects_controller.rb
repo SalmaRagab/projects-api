f5b4b28d6bd6d236051e193733f578b7f516a435
SalmaRagab <salmaahragab@gmail.com>
2022-11-22 23:06:05 +0100

    add api call to assign and un assign participants from a project

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,5 +1,5 @@
 class ProjectsController < ApplicationController
-    before_action :set_project, only: [:update]
+    before_action :set_project, only: [:update, :assign_participants]
 
     def index
         projects = Project.all
@@ -27,10 +27,24 @@ class ProjectsController < ApplicationController
         end
     end
 
+    def assign_participants
+        if @project.update(project_participants_params)
+            render json: @project
+        else
+            render json: {
+                reasons: @project.errors
+            }, status: :unprocessable_entity
+        end
+    end
+
     private
 
     def project_params
         params.require(:project).permit(:name, :owner_id, :state)
+    end
+
+    def project_participants_params
+        params.permit(project_participants_attributes: [:id, :_destroy, :participant_id])
     end
 
     def set_project
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
-  resources :projects, only: [:index, :create, :update]
+  resources :projects, only: [:index, :create, :update] do
+    member do
+      put "assign_participants" => "projects#assign_participants"
+    end
+  end
 end
